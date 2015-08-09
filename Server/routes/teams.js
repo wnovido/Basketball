41be4b24cbcc68d8203d987945afe9ec6ab6a356
Wilson Novido <wilson_novido@yahoo.com>
2015-08-08 23:36:46 -0400

    Add Teams REST API

--- Server/routes/teams.js
+++ Server/routes/teams.js
@@ -6,42 +6,41 @@ router.get('/', function(req, res) {
   // res.send('respond with a resource xxx' + tmp);
 	var data = {
 	    "error":1,
-	    "Users":""
+	    "Teams":""
 	};
 
-	connection.query("SELECT * from user_login",function(err, rows, fields) {
+	connection.query("SELECT * from teams",function(err, rows, fields) {
 	    if(rows.length != 0) {
 	        data["error"] = 0;
-	        data["Users"] = rows;
+	        data["Teams"] = rows;
 	        res.json(data);
 	    } else {
-	        data["Users"] = 'No Users Found..';
+	        data["Teams"] = 'No Teams Record Found..';
 	        res.json(data);
 	    }
 	});
 });
 
 router.post('/',function(req,res){	
-    var Email = req.body.email;
-    var Password = md5(req.body.password);
+    var Name = req.body.name;
     var data = {
         "error":1,
-        "Users":""
+        "Teams":""
     };
 
-    if (!!Email && !!Password) {
-        connection.query("INSERT INTO user_login (user_email, user_password) VALUES(?,?)",[Email,Password],function(err, rows, fields){
+    if (!!Name && !!Logo) {
+        connection.query("INSERT INTO teams (name, logo) VALUES(?,?)",[Name,Logo],function(err, rows, fields){
             if (!!err) {
-                data["Users"] = "Error Adding data: " + err;
+                data["Teams"] = "Error Adding data: " + err;
                 console.log(err);
             } else {
                 data["error"] = 0;
-                data["Users"] = "User Added Successfully";
+                data["Teams"] = "Team Record Added Successfully";
             }
             res.json(data);
         });
     } else {
-        data["Users"] = "Please provide all required data (i.e : Email, Password)";
+        data["Teams"] = "Please provide all required data (i.e : Name, Logo)";
         res.json(data);
     }
 });
@@ -49,26 +48,26 @@ router.post('/',function(req,res){
 
 router.put('/',function(req,res){
     var Id = req.body.id;
-    var Email = req.body.email;
-    var Password = md5(req.body.password);
+    var Name = req.body.name;
+    var Logo = req.body.logo;
     var data = {
         "error":1,
-        "Users":""
+        "Teams":""
     };
 
-    if(!!Id && !!Email && !!Password){
-        connection.query("UPDATE user_login SET user_email=?, user_password=? WHERE user_id=?",[Email,Password,Id],function(err, rows, fields){
+    if(!!Id && !!Email && !!Logo){
+        connection.query("UPDATE teams SET name=?, logo=? WHERE team_id=?",[Name,Logo,Id],function(err, rows, fields){
             if(!!err) {
-                data["Users"] = "Error Updating data: " + err;
+                data["Teams"] = "Error Updating data: " + err;
                 console.log(err);
             } else {
                 data["error"] = 0;
-                data["Users"] = "Updated User Successfully";
+                data["Teams"] = "Updated Team Record Successfully";
             }
             res.json(data);
         });
     }else{
-        data["Users"] = "Please provide all required data (i.e : id, Bookname, Authorname, Price)";
+        data["Teams"] = "Please provide all required data (i.e : id, Team, Logo)";
         res.json(data);
     }
 });
@@ -78,49 +77,48 @@ router.delete('/',function(req,res){
     var Id = req.body.id;
     var data = {
         "error":1,
-        "Users":""
+        "Teams":""
     };
     if (!!Id){
-        connection.query("DELETE FROM user_login WHERE user_id=?",[Id],function(err, rows, fields){
+        connection.query("DELETE FROM teams WHERE team_id=?",[Id],function(err, rows, fields){
             if(!!err) {
-                data["Users"] = "Error deleting data: " + err;
+                data["Teams"] = "Error deleting data: " + err;
                 console.log(err);
             } else {
                 data["error"] = 0;
-                data["Users"] = "Delete Book Successfully";
+                data["Teams"] = "Delete Book Successfully";
             }
             res.json(data);
         });
     } else {
-        data["Users"] = "Please provide all required data (i.e : id )";
+        data["Teams"] = "Please provide all required data (i.e : id )";
         res.json(data);
     }
 });
 
 
-router.get("/:user_id",function(req,res) {
-	var Id = req.params.user_id
+router.get("/:team_id",function(req,res) {
+	var Id = req.params.team_id
     var data = {
         "error":1,
-        "Users":""
+        "Teams":""
     };
     if (!!Id){
-        connection.query("SELECT * FROM user_login WHERE user_id=?",[Id],function(err, rows, fields){
+        connection.query("SELECT * FROM teams WHERE team_id=?",[Id],function(err, rows, fields){
             if(!!err) {
-                data["Users"] = "Error selecting data: " + err;
+                data["Teams"] = "Error selecting data: " + err;
                 console.log(err);
             } else {
                 data["error"] = 0;
-                data["Users"] = rows;
+                data["Teams"] = rows;
             }
             res.json(data);
         });
     } else {
-        data["Users"] = "Please provide all required data (i.e : id )";
+        data["Teams"] = "Please provide all required data (i.e : id )";
         res.json(data);
     }
 });
 
 
 module.exports = router;
-\
