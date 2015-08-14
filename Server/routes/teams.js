37b12fa47cc035a70214122d1b0dd9458cfd53be
Wilson Novido <wilson_novido@yahoo.com>
2015-08-13 23:30:44 -0400

    Fix Server Teams REST API

--- Server/routes/teams.js
+++ Server/routes/teams.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
+// CORS
 router.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -10,127 +11,103 @@ router.use(function(req, res, next) {
 
 /* GET users listing. */
 router.get('/', function(req, res) {
-	var data = {
-	    "error":1,
-	    "Teams":""
-	};
-
-	connection.query("SELECT * from teams",function(err, rows, fields) {
-        if (err) 
+	connection.query("SELECT * FROM teams",function(err, rows) {
+        if (rows.length == 0) {
             return res.status(404).send({message: 'No Teams Found'});
-        else
-            return res.jsonp(rows);
-
-	    /*if(rows.length != 0) {
-	        data["error"] = 0;
-	        data["Teams"] = rows;
-	        res.jsonp(rows);
-	    } else {
-	        data["Teams"] = 'No Teams Record Found..';
-	        res.jsonp(res.status(404).send({message: 'No Teams Found'}));
-            // return res.status(404).send({message: 'Tab Not Found'});  
-	    }*/
-
+            console.log(err);
+        } else {
+            res.jsonp(rows);
+        }
 	});
 });
 
 router.post('/',function(req,res){	
     var Name = req.body.name;
     var Logo = req.body.logo;
-    var data = {
-        "error":1,
-        "Teams":""
-    };
 
     if (!!Name && !!Logo) {
-        connection.query("INSERT INTO teams (name, logo) VALUES(?,?)",[Name,Logo],function(err, rows, fields){
+        connection.query("INSERT INTO teams (name, logo) VALUES(?,?)",[Name,Logo],function(err, rows){
             if (!!err) {
-                data["Teams"] = "Error Adding data: " + err;
+                return res.status(406).send({message: 'Error Adding data: ' + err});
                 console.log(err);
             } else {
-                data["error"] = 0;
-                data["Teams"] = "Team Record Added Successfully";
-            }
-            res.json(data);
+                connection.query("SELECT * FROM teams WHERE team_id=?",[rows.insertId],function(err, rows) {
+                    res.jsonp(rows);
+                });
+            }            
         });
     } else {
-        data["Teams"] = "Please provide all required data (i.e : Name, Logo)";
-        res.json(data);
+        return res.status(412).send({message: 'Please provide all required data (i.e : Name, Logo)'});
     }
 });
 
 
-router.put('/',function(req,res){
+router.put('/',function(req,res) {
     var Id = req.body.id;
     var Name = req.body.name;
     var Logo = req.body.logo;
-    var data = {
-        "error":1,
-        "Teams":""
-    };
 
-    if(!!Id && !!Name && !!Logo){
-        connection.query("UPDATE teams SET name=?, logo=? WHERE team_id=?",[Name,Logo,Id],function(err, rows, fields){
-            if(!!err) {
-                data["Teams"] = "Error Updating data: " + err;
+    if(!!Id && !!Name && !!Logo) {
+        connection.query("SELECT * FROM teams WHERE team_id=?",[Id],function(err, rows) {
+            if (rows.length == 0) {
+                return res.status(404).send({message: 'No Team Found With Id=' + Id});
                 console.log(err);
             } else {
-                data["error"] = 0;
-                data["Teams"] = "Updated Team Record Successfully";
+                connection.query("UPDATE teams SET name=?, logo=? WHERE team_id=?",[Name,Logo,Id],function(err, rows) {
+                    if(!!err) {
+                        return res.status(406).send({message: 'Error Updating data: ' + err});
+                        console.log(err);
+                    } else {
+                        res.jsonp(rows);
+                    };
+                });
             }
-            res.json(data);
         });
-    }else{
-        data["Teams"] = "Please provide all required data (i.e : id, Team, Logo)";
-        res.json(data);
+    } else {
+        return res.status(412).send({message: 'Please provide all required data (i.e : Id, Name, Logo)'});
     }
 });
 
 
 router.delete('/',function(req,res){
     var Id = req.body.id;
-    var data = {
-        "error":1,
-        "Teams":""
-    };
-    if (!!Id){
-        connection.query("DELETE FROM teams WHERE team_id=?",[Id],function(err, rows, fields){
-            if(!!err) {
-                data["Teams"] = "Error deleting data: " + err;
+
+    if (!!Id) {
+        connection.query("SELECT * FROM teams WHERE team_id=?",[Id],function(err, rows) {
+            if (rows.length == 0) {
+                return res.status(404).send({message: 'No Team Found With Id=' + Id});
                 console.log(err);
             } else {
-                data["error"] = 0;
-                data["Teams"] = "Deleted Teams Record Successfully";
+                connection.query("DELETE FROM teams WHERE team_id=?",[Id],function(err, rows2) {
+                    if (!!err) {
+                        return res.status(406).send({message: 'Error Deleting data: ' + err});
+                        console.log(err);
+                    } else {
+                        res.jsonp(rows);
+                    }
+                });
             }
-            res.json(data);
         });
     } else {
-        data["Teams"] = "Please provide all required data (i.e : id )";
-        res.json(data);
+        return res.status(412).send({message: 'Please provide all required data (i.e : Id)'});        
     }
 });
 
 
 router.get("/:team_id",function(req,res) {
 	var Id = req.params.team_id
-    var data = {
-        "error":1,
-        "Teams":""
-    };
+
     if (!!Id){
-        connection.query("SELECT * FROM teams WHERE team_id=?",[Id],function(err, rows, fields){
-            if(!!err) {
-                data["Teams"] = "Error selecting data: " + err;
+        connection.query("SELECT * FROM teams WHERE team_id=?",[Id],function(err, rows) {
+            if (rows.length == 0) {
+                return res.status(404).send({message: 'No Team Found With Id=' + Id});
                 console.log(err);
             } else {
-                data["error"] = 0;
-                data["Teams"] = rows;
+                res.json(rows);
             }
-            res.json(data);
         });
     } else {
-        data["Teams"] = "Please provide all required data (i.e : id )";
-        res.json(data);
+        return res.status(412).send({message: 'Please provide all required data (i.e : Id)'});        
     }
 });
 
