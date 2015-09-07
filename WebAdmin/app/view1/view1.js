5ac934ddf16e2533f42dcf325cda382135fa3516
Wilson Novido <wilson_novido@yahoo.com>
2015-09-07 10:28:26 -0400

    Applying Coding Standard: https://github.com/toddmotto/angularjs-styleguide

--- WebAdmin/app/view1/view1.js
+++ WebAdmin/app/view1/view1.js
@@ -1,8 +1,50 @@
 'use strict';
 
+(function() {
+function TeamController(TeamService, _,$loading, $timeout, $q, $alert) {
+    this.teams = TeamService.query();
+
+    var init = function() {
+        _.keys(this);
+    };
+
+    init();
+
+    /*   var $that = this;
+
+     $scope.loading = new $loading({
+     busyText: 'Loading proccess running...',
+     theme: 'danger',
+     timeout: false,
+     delayHide: 1000
+     });
+
+     $scope.loadingButtonSucces = function () {
+     return $timeout(function () {
+     return true;
+     }, 2000)
+     };
+
+     $scope.getError = function () {
+     var deferred = $q.defer();
+
+     setTimeout(function () {
+     deferred.reject('Error');
+     }, 1000);
+     return deferred.promise;
+     };
+
+     $scope.displayAlert = function (title, message, theme) {
+     $alert(message, title, theme)
+     };
+     */
+
+}
+
+
 angular.module('myApp.view1', ['ui.router'])
 
-.config(function($stateProvider, $urlRouterProvider) {
+.config(function configTeam($stateProvider, $urlRouterProvider) {
       $stateProvider
         .state('route1', {
             url: "/route1",
@@ -16,52 +58,7 @@ angular.module('myApp.view1', ['ui.router'])
           })
 })          
 
-.controller('View1Ctrl', ['$scope', 'TeamService', '_', '$loading', '$timeout', '$q', '$alert',
-    function TeamController($scope, TeamService, _,$loading, $timeout, $q, $alert) {
-        // $scope.items = ["A", "List", "Of", "Items"];
-        var tcx = this;
-        tcx.teams = TeamService.query();
-
-        var init = function() {
-            _.keys(tcx);
-        };
-
-        init();
-
-     /*   var $that = this;
-
-        $scope.loading = new $loading({
-            busyText: 'Loading proccess running...',
-            theme: 'danger',
-            timeout: false,
-            delayHide: 1000
-        });
-
-        $scope.loadingButtonSucces = function () {
-            return $timeout(function () {
-                return true;
-            }, 2000)
-        };
-
-        $scope.getError = function () {
-            var deferred = $q.defer();
-
-            setTimeout(function () {
-                deferred.reject('Error');
-            }, 1000);
-            return deferred.promise;
-        };
-
-        $scope.displayAlert = function (title, message, theme) {
-            $alert(message, title, theme)
-        };
-*/
-
-    }
-
-]);
+.controller('View1Ctrl', ['TeamService', '_', '$loading', '$timeout', '$q', '$alert', TeamController]);
 
 
-
-
-
+})();
